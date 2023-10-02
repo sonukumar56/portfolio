49acdf9602ce01cf7a3abca6ff350fe7e3f00a22
sonu kumar <sonukumar7505891@gmail.com>
2023-10-02 22:18:44 +0530

    background image add

--- lib/main.dart
+++ lib/main.dart
@@ -13,135 +13,142 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 120, left: 30),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50, backgroundImage: AssetImage("images/skb.jpg"),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Sonu Kumar",
-                        style: TextStyle(fontSize: 30,),
-                      ),
-                      Text(
-                        "Android Developer",
-                        style: TextStyle(fontSize: 15,fontFamily: "font/Roboto-Light.ttf"),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: Column(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("image/back.jpg")
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 120, left: 30),
+          child: Column(
+            children: <Widget>[
+              Row(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.school,
-                        size: 35,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Text(
-                        "Eshan college of Engineering",
-                        style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
-                      )
-                    ],
+                  CircleAvatar(
+                    radius: 50, backgroundImage: AssetImage("images/skb.jpg"),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.computer_rounded,
-                        size: 35,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Text(
-                        "B-tech In Computer Science",
-                        style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.location_pin,
-                        size: 35,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Text(
-                        "Agra (Uttar Predesh)",
-                        style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        size: 35,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Text(
-                        "sonukumar7501@gmail.com",
-                        style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        size: 35,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Text(
-                        "+917505891902",
-                        style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
-                      )
-                    ],
-                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Sonu Kumar",
+                          style: TextStyle(fontSize: 30,),
+                        ),
+                        Text(
+                          "Android Developer",
+                          style: TextStyle(fontSize: 15,fontFamily: "font/Roboto-Light.ttf"),
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
-            ),
-            SizedBox(height: 25,),
-            Text("Hello guys i am a new software enggineer and i am passinated to learn new things ",style: TextStyle(fontSize: 20,fontFamily: "font/Roboto-Medium.ttf"),),
-          SizedBox(height: 80,),
-            Text("Created By Sonu")
-          ],
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.school,
+                          size: 35,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Eshan college of Engineering",
+                          style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.computer_rounded,
+                          size: 35,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "B-tech In Computer Science",
+                          style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_pin,
+                          size: 35,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Agra (Uttar Predesh)",
+                          style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          size: 35,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "sonukumar7501@gmail.com",
+                          style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          size: 35,
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "+917505891902",
+                          style: TextStyle(fontSize: 18,fontFamily: "font/Roboto-Medium.ttf"),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 25,),
+              Text("Hello guys i am a new software enggineer and i am passinated to learn new things ",style: TextStyle(fontSize: 20,fontFamily: "font/Roboto-Medium.ttf"),),
+            SizedBox(height: 80,),
+              Text("Created By Sonu")
+            ],
+          ),
         ),
       ),
     );
